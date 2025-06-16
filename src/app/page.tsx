caadaf424ce3723dec1e41d2cb773a49dfa74ed3
Rafe Loveday <rafe.loveday@icloud.com>
2025-06-16 13:25:31 +0100

    hero page logo, oneliner, description and launch app button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import LiquidBackground from "@/components/ui/LiquidBackground";
-
+import LaunchAppButton from "@/components/ui/button/LaunchApp";
 
 export default function Home() {
   return (
@@ -9,10 +9,24 @@ export default function Home() {
       <LiquidBackground />
       <div className={styles.NavBar}>
         <div className={styles.NavBarTextContainer}>
-          <p className={styles.NavBarText}>Find your reports | Stake for insights</p>
+          <p className={styles.NavBarText}>Explore KOL reports | Stake for insights</p>
         </div>
         <div className={styles.NavBarButtonContainer}>
-          <button className={styles.NavBarButton}>Launch App</button>
+          <LaunchAppButton />
+        </div>
+      </div>
+
+      <div className={styles.HeroContainer}>
+        <div className={styles.LogoContainer}>
+          <Image src="/logos/Stratos Bar logo (White).png" alt="Stratos Bar Logo" width={200} height={200} objectFit="contain" className={styles.Logo} />
+          <p className={styles.LogoText}>Stratos scans the chatter. You stake for insights.</p>
+        </div>
+
+        <div className={styles.HeroDescriptionContainer}>
+          <p className={styles.HeroDescription}>Our AI cuts through the noise - tracking sentiment, surfacing events, and scoring voices that matter - so you don't have to waste hours researching.
+            Access clean, actionable reports built from thousands of tweets in seconds.
+          </p>
+          <LaunchAppButton />
         </div>
       </div>
     </main>
---
+++ src/components/ui/button/LaunchApp.tsx
@@ -0,0 +1,7 @@
+import styles from "./LaunchApp.module.css";
+
+export default function LaunchAppButton() {
+    return (
+        <button className={styles.NavBarButton}>Launch App</button>
+    )
+}
