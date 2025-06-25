9027a34b15cd1746570ebe29a12052a945d4dbf9
Rafe Loveday <rafe.loveday@icloud.com>
2025-06-25 21:52:17 +0100

    reactive connect wallet icon

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -5,6 +5,7 @@ import { useState } from 'react';
 
 export default function Dashboard() {
     const [isProfileOpen, setIsProfileOpen] = useState(false);
+    const [isWalletConnectOpen, setIsWalletConnectOpen] = useState(false);
 
     const handleProfileClick = () => {
         setIsProfileOpen((prev) => !prev);
@@ -23,8 +24,18 @@ export default function Dashboard() {
                             overflow: 'hidden',
                         }}
                     >
-                        <div className={styles.WalletConnectIndicator}></div>
-                        <div className={styles.WalletConnectText}>Connect Wallet</div>
+                        <div className={styles.WalletConnectIndicator} style={{
+                            opacity: isProfileOpen ? 0 : 0.8,
+                            transition: 'opacity 0.2s ease-in-out',
+                            background: isWalletConnectOpen ? 'green' : 'red',
+                            boxShadow: isWalletConnectOpen ? '0 0 10px 0 rgba(17, 142, 40, 0.816)' : '0 0 10px 0 rgba(167, 19, 19, 0.816)',
+                        }}></div>
+                        <div className={styles.WalletConnectText}
+                            style={{
+                                opacity: isProfileOpen ? 0 : 1,
+                                transition: 'opacity 0.2s ease-in-out',
+                            }}
+                        >{isWalletConnectOpen ? 'Connected' : 'Connect Wallet'}</div>
                     </div>
                     <div className={styles.Profile}>
                         <Image src="/icons/pfp-icon.webp" alt="Profile" width={90} height={90} objectFit='contain' className={styles.ProfileImage} onClick={handleProfileClick} />
