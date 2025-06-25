8028fd3eb7dfdfcd4f77e09a21d251371ccd0ea4
Rafe Loveday <rafe.loveday@icloud.com>
2025-06-25 23:03:44 +0100

    wallet connect panel closed by default

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -42,15 +42,16 @@ export default function Dashboard() {
                 <div className={styles.ProfileContainer}>
                     <div className={styles.WalletConnectContainer}
                         style={{
-                            width: isProfileOpen ? '0px' : '200px',
-                            height: isProfileOpen ? '0px' : '50px',
+                            width: isProfileOpen ? '200px' : '0px',
+                            height: isProfileOpen ? '50px' : '0px',
                             transition: 'all 0.3s ease-in-out',
                             overflow: 'hidden',
+                            visibility: isProfileOpen ? 'visible' : 'hidden',
                         }}
                         onClick={handleWalletClick}
                     >
                         <div className={styles.WalletConnectIndicator} style={{
-                            opacity: isProfileOpen ? 0 : 0.8,
+                            opacity: isProfileOpen ? 0.8 : 0,
                             transition: 'opacity 0.2s ease-in-out',
                             background: isWalletConnectOpen ? 'green' : 'red',
                             boxShadow: isWalletConnectOpen ? '0 0 10px 0 rgba(17, 142, 40, 0.816)' : '0 0 10px 0 rgba(167, 19, 19, 0.816)',
@@ -58,7 +59,7 @@ export default function Dashboard() {
                         <div className={styles.WalletConnectTextContainer}>
                             <div className={styles.WalletConnectText}
                                 style={{
-                                    opacity: isProfileOpen ? 0 : 1,
+                                    opacity: isProfileOpen ? 1 : 0,
                                     transition: 'opacity 0.2s ease-in-out',
                                 }}
                             >{isWalletConnectOpen ? 'Connected' : 'Connect Wallet'}</div>
